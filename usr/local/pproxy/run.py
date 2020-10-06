e3cb45a98142f28b07d3e7cc39e302629f2a7fcf
WEPN <support@we-pn.com>
2020-10-06 06:34:44 +0100

    issue #4: added check for the wipe-required case
    at boot, if device thinks it is claimed but server is reachable and it says otherwise, then wipe the device.
    
    also, added OOBE/startup logs to the error log collection for support tickets

--- usr/local/pproxy/run.py
+++ usr/local/pproxy/run.py
@@ -3,6 +3,8 @@ import time
 from oled import OLED as OLED
 from setup.onboard import OnBoard
 import requests, json
+import logging.config
+from device import Device
 
 
 try:
@@ -12,6 +14,11 @@ except ImportError:
 
 CONFIG_FILE='/etc/pproxy/config.ini'
 STATUS_FILE='/var/local/pproxy/status.ini'
+LOG_CONFIG="/etc/pproxy/logging.ini"
+logging.config.fileConfig(LOG_CONFIG,
+            disable_existing_loggers=False)
+logger = logging.getLogger("startup")
+logger.critical("Starting WEPN")
 
 oled = OLED()
 
@@ -21,47 +28,63 @@ status = configparser.ConfigParser()
 status.read(STATUS_FILE)
 oled.set_led_present(config.get('hw','led'))
 oled.show_logo()
-time.sleep(5)
+time.sleep(1)
+
+is_claimed = False
+server_checkin_done = False
+response = None
+url_address = config.get('django','url') + "/api/device/is_claimed/"
+data = json.dumps({'serial_number': config.get('django','serial_number')})
+headers = {'Content-Type': 'application/json'}
+try:
+  response = requests.post(url_address, data=data, headers=headers)
+  is_claimed = (response.status_code == 200)
+  jresponse = json.loads(response.content)
+  logger.error("is_claimed updated to " + str(is_claimed))
+  server_checkin_done = True
+except requests.exceptions.RequestException as exception_error:
+  logger.error("Error in connecting to server for claim status")
+
 
 if 1 == int(status.get('status','claimed')):
+    if not is_claimed and server_checkin_done:
+        # server says device is not claimed, so wipe it
+        logger.error("Server says unclaimed, locally cached claimed. Wiping")
+        status['status']['mqtt-reason'] = '0'
+        status['status']['claimed'] = '0'
+        status['status']['mqtt'] = '0'
+        status['status']['state'] = '3'
+        with open(STATUS_FILE, 'w') as statusfile:
+           status.write(statusfile)
+        #reboot to go into onboarding
+        device = Device(logger)
+        device.reboot()
+
     while True:
           try:
              PPROXY_PROCESS = PProxy()
              PPROXY_PROCESS.start()
           except:
               del(PPROXY_PROCESS) 
-              print("Retrying in 60 seconds ....")
+             logger.debug("Retrying in 60 seconds ....")
               time.sleep(60)
               continue
           break
 else:
     while True:
-          is_claimed = False 
-          response = None
-          url_address = config.get('django','url') + "/api/device/is_claimed/"
-          data = json.dumps({'serial_number': config.get('django','serial_number')})
-          headers = {'Content-Type': 'application/json'}
-          try:
-              response = requests.post(url_address, data=data, headers=headers)
-              is_claimed = (response.status_code == 200)
-              jresponse = json.loads(response.content)
-              print("is_claimed updated to " + str(is_claimed))
-          except requests.exceptions.RequestException as exception_error:
-              print("Error in connecting to server for claim status")
           try:
              ONBOARD = OnBoard()
              if is_claimed:
                  for name, key in status.items("previous_keys"):
-                     print("Trying an old key: " + name + " , " + key)
+                     logger.debug("Trying an old key: " + name + " , " + key)
                      ONBOARD.set_rand_key(key)
                      ONBOARD.start(True)
              ONBOARD.start()
           except Exception as e:
-              print("Exception in onboarding")
-              print(e)
+              logger.error("Exception in onboarding:" + str(e))
               if ONBOARD:
               del(ONBOARD) 
-              print("Retrying in 60 seconds ....")
+              logger.debug("Retrying in 60 seconds ....")
               time.sleep(60)
               continue
           break
