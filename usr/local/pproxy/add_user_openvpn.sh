9d43ebf8de6066240c211c5b30e374d9b1ae006d
WEPN <support@we-pn.com>
2022-01-29 11:15:45 -0800

    add some extra sanitization to the openvpn scripts

--- usr/local/pproxy/add_user_openvpn.sh
+++ usr/local/pproxy/add_user_openvpn.sh
@@ -1,21 +1,31 @@
 #!/bin/sh
+
+if (( $# < 3 ))
+then
+	echo "Format: $0 username ip port"
+	exit 1
+fi
+USERNAME=${1//[^a-zA-Z0-9_\.]/}
+IP=${2//[^0-9_\.]/}
+PORT=${3//[^0-9_]/}
+
 CONFIG_FILE_PATH=/var/local/pproxy/
-FILE=$CONFIG_FILE_PATH/$1.ovpn
+FILE=$CONFIG_FILE_PATH/$USERNAME.ovpn
 cd /etc/openvpn/easy-rsa/
-./easyrsa build-client-full $1 nopass
+./easyrsa build-client-full $USERNAME nopass
 
 # Generates the custom client.ovpn
 cp /etc/openvpn/client-common.txt $FILE
-echo "remote $2 $3" >> $FILE
+echo "remote $IP $PORT" >> $FILE
 echo "<ca>" >> $FILE
 cat /etc/openvpn/easy-rsa/pki/ca.crt >> $FILE
 echo "</ca>" >> $FILE
 echo "<cert>" >> $FILE
-cat /etc/openvpn/easy-rsa/pki/issued/$1.crt >> $FILE
+cat /etc/openvpn/easy-rsa/pki/issued/$USERNAME.crt >> $FILE
 echo "</cert>" >> $FILE
 echo "<key>" >> $FILE
-cat /etc/openvpn/easy-rsa/pki/private/$1.key >> $FILE
+cat /etc/openvpn/easy-rsa/pki/private/$USERNAME.key >> $FILE
 echo "</key>" >> $FILE
 
 chown pproxy.pproxy $FILE
-echo "Done generating $1"
+echo "Done generating $USERNAME"
--- usr/local/pproxy/delete_user_openvpn.sh
+++ usr/local/pproxy/delete_user_openvpn.sh
@@ -1,14 +1,20 @@
 #!/bin/sh
+if (( $# < 1 ))
+then
+	echo "Format: $0 username"
+	exit 1
+fi
 cd /etc/openvpn/easy-rsa/
-./easyrsa --batch revoke $1
+USERNAME=${1//[^a-zA-Z0-9_\.]/}
+./easyrsa --batch revoke $USERNAME
 ./easyrsa gen-crl
-rm -rf pki/reqs/$1.req
-rm -rf pki/private/$1.key
-rm -rf pki/issued/$1.crt
+rm -rf pki/reqs/$USERNAME.req
+rm -rf pki/private/$USERNAME.key
+rm -rf pki/issued/$USERNAME.crt
 rm -rf /etc/openvpn/crl.pem
 cp /etc/openvpn/easy-rsa/pki/crl.pem /etc/openvpn/crl.pem
 #if user is currently connected, restart server so they get kicked out
-if grep "$1" /etc/openvpn/openvpn-status.log
+if grep "$USERNAME" /etc/openvpn/openvpn-status.log
 then
     /etc/init.d/openvpn restart
 fi
