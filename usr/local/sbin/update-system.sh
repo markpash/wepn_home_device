89da86c03a806511bb1870e452ea861dcb97cd56
WEPN <support at we-pn .com>
2022-07-16 18:11:26 -0700

    adding update mechanism for ca certificates; used to be done ia apt

--- usr/local/sbin/update-system.sh
+++ usr/local/sbin/update-system.sh
@@ -3,9 +3,13 @@
 #sleep 300
 date -R > /var/local/pproxy/last-system-update 2>&1
 
+# get new certificates for roots
+/usr/sbin/update-ca-certificates
+
 dpkg --configure -a
 
 wget -qO - https://repo.we-pn.com/repo.we-pn.com.gpg.key | sudo apt-key add -
+
 /usr/bin/apt-get update
 /usr/bin/apt-get update -y --allow-releaseinfo-change --fix-missing
 /usr/bin/apt-get --yes  -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" upgrade >> /tmp/update-out 2>&1
