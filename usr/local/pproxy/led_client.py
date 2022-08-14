8ebaafb4b8f4d4dc5e800b5a1bd958e950feb99a
WEPN PPROXY User <pproxy@wepn>
2022-08-14 16:20:43 -0700

    updated test pattens and set brightness method

--- usr/local/pproxy/led_client.py
+++ usr/local/pproxy/led_client.py
@@ -12,7 +12,6 @@ logging.config.fileConfig(LOG_CONFIG,
 logger = logging.getLogger("led_client")
 
 
-
 class LEDClient:
     ''' The nethods of this class send commands to the LED manager,
     which runs with root previlages due to hardware DMA security constraints.
@@ -24,6 +23,7 @@ class LEDClient:
     The LED client is a Python script that runs as a non-root user. The LED client function
     is to serialize LED commands and send them to the LED manager in a secure manner.
     '''
+
     def __init__(self):
         self.client = None
         if os.path.exists(LM_SOCKET_PATH):
@@ -39,16 +39,28 @@ class LEDClient:
             return
         self.send("set_enabled " + str(int(enabled)))
 
+    def set_brightness(self, brightness=0.5):
+        '''
+        sets led brightness
+        '''
+        if self.client is None:
+            return
+        if brightness < 0 or brightness > 1:
+            print("brightness must be between 0 and 1")
+            return
+        self.send("set_brightness " +
+                  str(brightness))
+
     def send(self, cmd):
         self.client.send(cmd.encode('utf-8'))
 
-    def set_all(self, color = (255,255,255)):
+    def set_all(self, color=(255, 255, 255)):
         '''
         sets all LEDs to the specified color
         '''
         if self.client is None:
             return
-        (r,g,b) = color
+        (r, g, b) = color
         self.send("set_all " +
                   str(r) + " " +
                   str(g) + " " +
@@ -68,26 +80,27 @@ class LEDClient:
         if self.client is None:
             return
         self.send("rainbow " +
-                str(rounds) + " " +
-                str(wait))
+                  str(rounds) + " " +
+                  str(wait))
 
-    def progress_wheel_step(self, color = (255,255,255)):
+    def progress_wheel_step(self, color=(255, 255, 255)):
         '''
         increments the position of bright strip by one step.
         '''
         if self.client is None:
             return
-        (r,g,b) = color
+        (r, g, b) = color
         self.send("progress_wheel_step " +
                   str(r) + " " +
                   str(g) + " " +
                   str(b))
-    def pulse(self, color = (255,255,255),
-                    wait = 100,
-                    repetitions = 5):
+
+    def pulse(self, color=(255, 255, 255),
+              wait=100,
+              repetitions=5):
         if self.client is None:
             return
-        (r,g,b) = color
+        (r, g, b) = color
         self.send("pulse " +
                   str(r) + " " +
                   str(g) + " " +
@@ -95,12 +108,12 @@ class LEDClient:
                   str(wait) + " " +
                   str(repetitions))
 
-    def blink(self, color = (255,255,255),
-                    wait = 100,
-                    repetitions = 5):
+    def blink(self, color=(255, 255, 255),
+              wait=100,
+              repetitions=5):
         if self.client is None:
             return
-        (r,g,b) = color
+        (r, g, b) = color
         self.send("blink " +
                   str(r) + " " +
                   str(g) + " " +
@@ -109,13 +122,13 @@ class LEDClient:
                   str(repetitions))
 
     def spinning_wheel(self,
-                        color = (255,255,255),
-                        wait = 100,
-                        length = 5,
-                        repetitions = 5):
+                       color=(255, 255, 255),
+                       wait=100,
+                       length=5,
+                       repetitions=5):
         if self.client is None:
             return
-        (r,g,b) = color
+        (r, g, b) = color
         self.send("spinning_wheel " +
                   str(r) + " " +
                   str(g) + " " +
@@ -125,11 +138,11 @@ class LEDClient:
                   str(repetitions))
 
     def progress_wheel(self,
-                        color = (255,255,255),
-                        percentage = 0.5):
+                       color=(255, 255, 255),
+                       percentage=0.5):
         if self.client is None:
             return
-        (r,g,b) = color
+        (r, g, b) = color
         self.send("progress_wheel " +
                   str(r) + " " +
                   str(g) + " " +
@@ -137,12 +150,12 @@ class LEDClient:
                   str(percentage))
 
     def fill_upto(self,
-                        color = (255,255,255),
-                        percentage = 0.5,
-                        wait = 100):
+                  color=(255, 255, 255),
+                  percentage=0.5,
+                  wait=100):
         if self.client is None:
             return
-        (r,g,b) = color
+        (r, g, b) = color
         self.send("fill_upto " +
                   str(r) + " " +
                   str(g) + " " +
@@ -151,12 +164,12 @@ class LEDClient:
                   str(wait))
 
     def fill_downfrom(self,
-                        color = (255,255,255),
-                        percentage = 0.5,
-                        wait = 100):
+                      color=(255, 255, 255),
+                      percentage=0.5,
+                      wait=100):
         if self.client is None:
             return
-        (r,g,b) = color
+        (r, g, b) = color
         self.send("fill_downfrom " +
                   str(r) + " " +
                   str(g) + " " +
@@ -166,58 +179,73 @@ class LEDClient:
 
 
 if __name__ == '__main__':
+    # add test cased for (1) enable and disable (2) set brightness
     lc = LEDClient()
     lc.set_enabled(True)
-    time.sleep(1)
-    lc.blank()
-    #fill_upto
-    lc.fill_upto((0,0,255), 1, 15)
-    time.sleep(2)
-    #fill_downfrom
-    lc.fill_downfrom((0,0,255), 1, 15)
-    time.sleep(5)
-    #magenta
-    lc.set_all((255, 0, 255))
-    time.sleep(2)
-    #yellow
-    lc.set_all((255, 255, 0))
-    time.sleep(2)
-    #cyan
-    lc.set_all((0, 255, 255))
-    time.sleep(2)
-    #white
     lc.set_all((255, 255, 255))
-    time.sleep(2)
-    #red
+    time.sleep(1)
+    print("white ring must glow for 1 second")
+    lc.set_enabled(False)
     lc.set_all((255, 0, 0))
-    time.sleep(2)
-    #green
+    print("red ring must NOT glow for 1 second")
+    time.sleep(1)
+    lc.set_enabled(True)
+    lc.blank()
+    # fill_upto
+    lc.fill_upto((0, 0, 255), 1, 20)
+    time.sleep(1)
+    # fill_downfrom
+    lc.fill_downfrom((0, 0, 255), 1, 20)
+    time.sleep(1)
+    # magenta
+    lc.set_all((255, 0, 255))
+    time.sleep(1)
+    # yellow
+    lc.set_all((255, 255, 0))
+    time.sleep(1)
+    # cyan
+    lc.set_all((0, 255, 255))
+    time.sleep(1)
+    # white
+    lc.set_all((255, 255, 255))
+    time.sleep(1)
+    # red
+    lc.set_all((255, 0, 0))
+    time.sleep(1)
+    # green
     lc.set_all((0, 255, 0))
-    time.sleep(2)
-    #blue
+    time.sleep(1)
+    # blue
     lc.set_all((0, 0, 255))
-    time.sleep(2)
-    # #turn off all
+    time.sleep(1)
+    # turn off all
     lc.blank()
     # activate rainbow
-    lc.rainbow(3, 5)
-    time.sleep(5)
+    lc.rainbow(10, 5)
+    time.sleep(1)
+    for i in range(0, 11, 1):
+        lc.set_brightness(i / 10)
+        time.sleep(0.2)
     # show progress wheel step
-    for i in range(10):
+    for i in range(15):
         lc.progress_wheel_step((0, 255, 0))
-        time.sleep(0.5)
+        time.sleep(0.1)
     # pulse
     lc.pulse((255, 0, 255), 50, 5)
-    time.sleep(5)
+    time.sleep(2)
     # blink
     lc.blink((255, 0, 0), 200, 5)
     time.sleep(1)
     # spinning wheel
     lc.spinning_wheel((255, 255, 255), 20, 10, 3)
-    time.sleep(10)
+    time.sleep(2)
     lc.blank()
-    #set progress wheel to 60%
+    # set progress wheel to 60%
     for i in range(25):
-        lc.progress_wheel((0, 0, 255), i/25)
-        time.sleep(0.5)
+        lc.progress_wheel((0, 0, 255), i / 25)
+        time.sleep(0.1)
+    lc.blank()
+    time.sleep(1)
+    lc.rainbow(3, 5)
+    time.sleep(2)
     lc.blank()
