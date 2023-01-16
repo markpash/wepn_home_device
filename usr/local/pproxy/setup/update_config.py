5e91c800e87ec92615bb8a481d8742a373a1f054
WEPN <support@we-pn.com>
2023-01-15 22:42:41 -0800

    Minor updates, bumping up version

--- usr/local/pproxy/setup/update_config.py
+++ usr/local/pproxy/setup/update_config.py
@@ -188,7 +188,7 @@ if shadowsocks_3:
 else:
     config.set('shadow', 'method', 'aes-256-cfm')
 
-status.set('status', 'sw', '1.11.1')
+status.set('status', 'sw', '1.11.3')
 
 
 with open(CONFIG_FILE, 'w') as configfile:
