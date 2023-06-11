9c5d180dd5db992eaa6b0158a85418954a5437bf
WEPN <support@we-pn.com>
2023-06-11 13:47:48 -0700

    return sources.list to original for 64 bit

--- usr/local/sbin/upgrade-os.sh
+++ usr/local/sbin/upgrade-os.sh
@@ -1,7 +1,7 @@
 export DEBIAN_FRONTEND=noninteractive
 export DEBIAN_PRIORITY=critical
 
-DEST_VERSION_START=5
+DEST_VERSION_START=6
 UNAME=`uname -r`
 
 if [[ $UNAME == $DEST_VERSION_START* ]]; then
@@ -9,18 +9,28 @@ if [[ $UNAME == $DEST_VERSION_START* ]]; then
 	exit
 fi
 
+# Going forward after this, we need to *not* touch main repo lsits
+# and only update wepn.list
 cat > /etc/apt/sources.list << EOF
-deb http://raspbian.raspberrypi.org/raspbian/ bullseye main contrib non-free rpi
-# Uncomment line below then 'apt-get update' to enable 'apt-get source'
-#deb-src http://raspbian.raspberrypi.org/raspbian/ bullseye main contrib non-free rpi
-deb https://repo.we-pn.com/debian/ bullseye main
+deb http://deb.debian.org/debian bullseye main contrib non-free
+deb http://security.debian.org/debian-security bullseye-security main contrib non-free
+deb http://deb.debian.org/debian bullseye-updates main contrib non-free
+# Uncomment deb-src lines below then 'apt-get update' to enable 'apt-get source'
+#deb-src http://deb.debian.org/debian bullseye main contrib non-free
+#deb-src http://security.debian.org/debian-security bullseye-security main contrib non-free
+#deb-src http://deb.debian.org/debian bullseye-updates main contrib non-free
 EOF
+
 
 
 cat > /etc/apt/sources.list.d/raspi.list << EOF
 deb http://archive.raspberrypi.org/debian/ bullseye main
 # Uncomment line below then 'apt-get update' to enable 'apt-get source'
 #deb-src http://archive.raspberrypi.org/debian/ bullseye main
+EOF
+
+cat > /etc/apt/sources.list.d/wepn.list << EOF
+deb https://repo.we-pn.com/debian/ bullseye main
 EOF
 
 /bin/bash /usr/local/sbin/update-system.sh >> /tmp/update-out 2>&1
