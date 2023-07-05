4764b53b0a45db6b20e10ad98ec40eb1125ddab6
WEPN <support@we-pn.com>
2023-07-05 16:08:08 -0700

    adding functions to play animation

--- usr/local/pproxy/tests/lcd_test.py
+++ usr/local/pproxy/tests/lcd_test.py
@@ -1,13 +1,16 @@
-import lcd
-import time
 import sys
 import os
+import time
 up_dir = os.path.dirname(os.path.abspath(__file__)) + '/../'
 sys.path.append(up_dir)
+import lcd # noqa
 
 
 LCD = lcd.LCD()
 LCD.set_lcd_present(1)
+LCD.set_backlight(False)
+time.sleep(1)
+print("Shows this function is not working right: backlight is on?" + str(lcd.get_backlight_is_on()))
 # LCD.display([(1,"PIN:",0, "white"), (2,"2018727523",0i, "red"), (3,"zxcvbnmmm,./asdfghjkl;qwertyuiop",2,"greed")], 20)
 LCD.display([(1, "PIN:", 0, "white"), (2, "2018727523", 0, "red"),
             (3, "https://youtu.be/jYgeDSG9G0A", 2, "green")], 20)
@@ -15,8 +18,9 @@ input("PIN string shown, skip: Enter")
 LCD.display([(1, "https://youtu.be/jYgeDSG9G0A", 2, "green")], 20)
 input("only qr code shown, skip: Enter")
 
-# LCD.show_logo()
-#input("Logo shown, step: Enter")
+LCD.show_logo()
+LCD.play_animation("/etc/passwd", 2)
+input("Logo shown, step: Enter")
 for ch in range(30, 255):
     LCD.display([(1, str(ch), 0, "blue"), (2, chr(ch), 1, "blue")], 20)
     input(str(ch) + " shown ...")
