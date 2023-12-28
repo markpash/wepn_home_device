7319924012d80c6d384dd95ed9db031a49ea7ae6
WEPN <support@we-pn.com>
2023-12-27 21:30:09 -0800

    weekly updates do not upgrade OS, instead that's a monthly cron now

--- usr/local/sbin/update-pproxy.sh
+++ usr/local/sbin/update-pproxy.sh
@@ -8,6 +8,5 @@ dpkg --configure -a >> /tmp/update-out 2>&1
 /usr/bin/apt-get update >> /tmp/update-out 2>&1
 /usr/bin/apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew"  -f install pproxy-rpi >> /tmp/update-out 2>&1
 
-#OS upgrade needed?
-/usr/local/sbin/upgrade-os.sh >> /tmp/update-out 2>&1
-apt-get -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" install python3  -qy -f
+# Moved to cron-root: OS upgrade needed?
+# /usr/local/sbin/upgrade-os.sh >> /tmp/update-out 2>&1
